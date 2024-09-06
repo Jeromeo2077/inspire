d4590c23e660ba21b82b055d2da1bb6357fc5a00
Jerome Otto <jeromeo1969@hotmail.com>
2024-09-06 11:30:35 -0600

    Created BackgroundImagesController, BackgroundImagesService, BackgroundImages Model

--- app/App.js
+++ app/App.js
@@ -1,13 +1,15 @@
 import { AuthController } from './controllers/AuthController.js';
+import { BackgroundImagesController } from './controllers/BackgroundImagesController.js';
 import { router } from './router-config.js';
 const USE_ROUTER = false
 
 class App {
 
   AuthController = new AuthController()
-  
+  BackgroundImagesController = new BackgroundImagesController()
+
   constructor() {
-    if(USE_ROUTER){
+    if (USE_ROUTER) {
       this.router = router
       this.router.init(this)
     }
---
+++ app/controllers/BackgroundImagesController.js
@@ -0,0 +1,8 @@
+export class BackgroundImagesController {
+  constructor() {
+    console.log("BackgroundImagesController Loaded");
+
+
+  }
+
+}
