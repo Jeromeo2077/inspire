0d093119ee476c9a5fbc7ed8af9f8082779a4d58
Jerome Otto <jeromeo1969@hotmail.com>
2024-09-09 23:26:33 -0600

    Goodnight Commit

--- app/controllers/QuotesController.js
+++ app/controllers/QuotesController.js
@@ -1,9 +1,13 @@
+import { AppState } from "../AppState.js";
 import { quotesService } from "../services/QuotesService.js";
 import { Pop } from "../utils/Pop.js";
+import { setHTML } from "../utils/Writer.js";
 
 export class QuotesController {
   constructor() {
     console.log('QuotesController has loaded');
+
+    this.getQuotes()
 
   }
 
@@ -14,5 +18,22 @@ export class QuotesController {
       Pop.error(error);
       console.log(error);
     }
+  }
+
+  drawQuote() {
+    const quote = AppState.quote
+
+    let quoteHTML = ''
+
+    quoteHTML = `
+    <div>${quote.content}</div>
+    <div>${quote.author}</div>
+
+    `
+
+    setHTML('quote', quoteHTML)
+
+
+
   }
 }
